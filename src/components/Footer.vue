31f463e7f3b1a3fc8187d70357ed0f7ae80e35ee
112RG <112RG@users.noreply.github.com>
2022-07-11 21:41:32 +1000

    Update writing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 	<footer class="bg-discord-blurple-560">
 		<div class="max-w-3xl mx-auto text-center px-16 pt-10 pb-4 text-gray-200">
 			<strong><router-link to="/">deaftone</router-link></strong>
-			<p class="mb-4">A powerful library for interacting with the Discord API</p>
+			<p class="mb-4">A awesome free music server</p>
 			<Stats class="mb-4" />
 			<p class="text-xs break-words-legacy">
 				commit: {{ gitCommit }}<br />
